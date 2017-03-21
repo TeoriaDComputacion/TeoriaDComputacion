d47f58980e84c71d7a1329b5e739a7343f373142
Miguel Ballén <migaballengal@unal.edu.co>
2017-03-21 12:30:06 -0500

    Update Taller1.py
    Just variable names changed :)

--- Taller1.py
+++ Taller1.py
@@ -1,35 +1,57 @@
-import pylab as pl
+import pylab as pl   
 import numpy as np
 import math as m
 
-f  = -7. /8
+f1  = -7./8
 f2 =  1./8
-OM2 = (m.pi*2)*f2
-OM  = (m.pi*2)*f
+
+
+fang1 = (m.pi*2)*f1  
+fang2 = (m.pi*2)*f2  #Frecuencia angular
+
 FI = 0
-print f,f2,OM2,OM
+
+print "frecuencia -7/8: ", f1 
+print "frecuencia 1/8: ", f2 
+
+print "f.angular 1: ", fang1
+print "f.angular 2: ", fang2 
+
+#para que es esto? 
 def ooo(*a):
     return 0
-def sen(t,om,n=1):
-    return n*m.sin((om * t) + FI)
-def cos(t,om,n=1):
-    return n*m.cos((om * t) + FI)
+
+def sen(t, fang , Amp=1):
+    return Amp* m.sin((fang * t) + FI)
+
+def cos(t,fang ,Amp=1):
+    return Amp* m.cos((fang * t) + FI)
 
 
-def graf(F,om,i,v, x_0=-10., x_f=11.,n=1.):
+ #funcion, frecuencia angular, pasos de 0,001, color, inicio, fin
+def graf(f, fang, i, color, x_0=-10, x_f=11, A=1):
+
     x = np.arange(x_0, x_f, i)
-    y = [F(l,om,n) for l in x]
+    y = [f( l, fang, A) for l in x]
 
     if x_f > 1: pl.xticks(np.arange(x_f))
-    pl.plot(x,y, v)
+
+    pl.plot(x, y, color)
     pl.xlabel("Hz")
     pl.ylabel("f(x)")
     pl.grid(True)
+
 ##primer Punto
-graf(ooo,0,1.,"-",0,12)
-graf(sen,OM,0.001,"-",0)#sen con frecuencia de -7. /8
-graf(sen,OM2,0.1,"-",0) #sen con frecuencia de 1. /8
-graf(sen,OM,1.,"o",0) #senalmuestreada a un Hz
+
+ 	#funcion, w, pasos de 0,001, color, inicio, fin
+graf(sen, fang1, 0.001, '-g', 0, 12)#sen con frecuencia de -7. /8,
+graf(sen, fang2, 0.001, '-b', 0, 12) #sen con frecuencia de 1. /8
+graf(sen, fang1, 1, '-r', 0, 12) #senalmuestreada a un Hz
+
+
+
+
+
 
 
 pl.show()
@@ -57,6 +79,7 @@ pl.show()
 ###Segundo Punto
 #la frecuencia de muestreo para evitar el aliasing
 #   Si fx = 200 Hz cual seria x(n)
+
 i= .0001;V="-";o_=0; f=.05
 graf(ooo,0,i,V,o_,f)
 graf(sen,200,i,V,o_,f,n=3)   #X sen(2.PI.200hzt)
@@ -72,3 +95,9 @@ graf(sen,75,i,V,o_,f,n=3)   #X sen(2.PI.75hzt)
 i=  1./150 ;V="o"
 graf(sen,75,i,V,o_,f,n=3)   #X  Muestreo a 400hz
 pl.show()
+
+
+
+
+
+
